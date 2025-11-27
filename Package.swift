56bf42679bcce6432cde14d870e027e46e9d2f43
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-27 21:46:10 +0100

    Fix Package.swift to use GitHub URLs instead of local paths

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.5.0"),
-        .package(path: "../swift-rfc-3986"),
-        .package(path: "../swift-rfc-5322"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-3986", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-5322", from: "0.0.1"),
     ],
     targets: [
         .target(
